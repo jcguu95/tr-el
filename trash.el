7ea921ac4312660999207ac51c4e6b57349ec49d
jcguu95 <jcguu95@gmail.com>
2021-02-27 09:05:20 -0600

    fix minor and added trash-dired-trash-marked-files

--- trash.el
+++ trash.el
@@ -14,32 +14,44 @@
 
 ;; TODO how to ensure transaction is either done or undone?
 (defstruct trash-record
-  timestamp file message memo)
+  timestamp files message memo)
 
 (defun trash-time-string ()
   "Return the current time string with timezone in the format
 that this program prefers."
   (format-time-string "%Y%m%dT%H%M%S%z"))
 
+(defun trash-write-record-to-db (trash-record)
+  "Write TRASH-RECORD into the database under *TRASH-DB*."
+  (unless (trash-record-p trash-record)
+    (error "Input must be a legt trash-record."))
+  (let* ((time (trash-record-timestamp trash-record))
+         (target (concat *trash-db* "/" time)))
+    (write-region (prin1-to-string trash-record) nil target)))
+
 (defun trash-trash-files (files)
   "Trash all files in the list FILES to the trash-store directory
-specified by the moment the operation is performed."
+specified by the moment the operation is performed. Make and
+timestamp a single trash-record, and write to the database."
   (let ((now (trash-time-string)))
     (loop for file in files
           do (let* ((abso (file-truename file))
                     (pail (concat *trash-store* "/" now))
                     (target (concat pail "/" (file-name-base abso))))
-
                (mkdir pail t)
-               (make-trash-record
-                :timestamp now :file abso
-                :message nil :memo "") ;; TODO Let user add memo by option.
-               (rename-file abso target) ;; TODO Better status report?
-               ))))
+               (trash-write-record-to-db
+                (make-trash-record
+                 :timestamp now :files files
+                 ;; TODO Add options to add messages and memos.
+                 :message nil :memo ""))
+               ;; TODO Better status report?
+               (rename-file abso target)))))
 
 (defun trash-dired-trash-marked-files ()
   "In dired, trash the marked files, or the file at point if none
 is marked, using #'trash-trash-files."
+  ;; TODO add options for user confirmation.
+  ;; TODO add message to confirm that operation is done.
   (interactive)
   (trash-trash-files (dired-get-marked-files)))
 
