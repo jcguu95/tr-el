0e7efef3d7d9bcf9015681d90215672704b85fb3
jcguu95 <jcguu95@gmail.com>
2021-02-28 09:30:36 -0600

    docstrings, options..

--- tr-dired.el
+++ tr-dired.el
@@ -1,10 +1,17 @@
 ;;; tr-dired.el -mode -*- coding: utf-8; lexical-binding: t; -*-
 
-(cl-defun tr-dired-trash-marked-files (&optional (memo ""))
+(cl-defun tr-dired-trash-marked-files
+    (&optional (memo "")
+               (confirm nil))
+  "To be called in dired. It trashes all marked files, or the
+file at point if no files are marked."
   (interactive)
   (tr-trash-files (dired-get-marked-files) memo))
 
-(cl-defun tr-dired-trash-file-at-point (&optional (memo ""))
+(cl-defun tr-dired-trash-file-at-point
+    (&optional (memo "")
+               (confirm nil))
+  "To be called in dired. It trashes the file at point."
   (interactive)
   (tr-trash-files (dired-get-filename) memo))
 
